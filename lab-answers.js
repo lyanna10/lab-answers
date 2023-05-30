dc852f49b92f2206f9874b3bcb785bea0cf44033
Lyann10 <glody.albadose@gmail.com>
2023-05-30 14:54:33 -0500

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -2,8 +2,23 @@
 /*for (let i = 1; i <= 20; i++) {
     console.log(i)
 }
-*/
+
 //Get Even - Write a for loop that will log only the even numbers in 0 through 200
 for (let i = 0; i < 200; i+=2) {
     console.log(i)
 }
+*/
+//Fizz Buzz
+let x = []
+for (let i = 1; i < 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        x.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+        x.push("Fizz");
+    } else if (i % 5 === 0) {
+        x.push("Buzz");
+    } else {
+        x.push(i)
+    }
+}
+console.log(x)
