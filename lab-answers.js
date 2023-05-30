995dc1fb9f39082a49b49713fffe55d117a810d0
Lyann10 <glody.albadose@gmail.com>
2023-05-30 15:47:23 -0500

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -7,7 +7,7 @@
 for (let i = 0; i < 200; i+=2) {
     console.log(i)
 }
-*/
+
 //Fizz Buzz
 let x = []
 for (let i = 1; i < 100; i++) {
@@ -22,3 +22,28 @@ for (let i = 1; i < 100; i++) {
     }
 }
 console.log(x)
+*/
+
+//Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+
+//Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2] = 5001;
+console.log(plantee);
+
+//Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City"
+console.log(wolfy);
+
+//Give D'Art a second hometown by adding "Hawkins"
+dart[4] = "Hawkins"
+console.log(dart)
+
+//Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+wolfy.splice(0, 1, 'Gameboy')
+console.log(wolfy)
+
